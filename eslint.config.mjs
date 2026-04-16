d41b794b0bfd8abbfc4a66c4b5f0d8e89d33383e
aurore.segovia <a.segovia@hookto.fr>
2026-04-16 15:55:47 +0200

    chore : add eslint for test

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+   {
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+      "no-console": "warn",
+    },
+  },
+)
