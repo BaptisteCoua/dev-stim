39c4c6bdc41c165689bbe5382cb3d270262ec72c
Aurore S <161458137+DameDanny@users.noreply.github.com>
2026-04-17 11:13:59 +0200

    Dsb chore install vuetify and i18n (#3)
    * chore: add vuetify and its defaults file
    
    * chore: install and configurate i18n
    
    * Apply Prettier format
    
    ---------
    
    Co-authored-by: aurore.segovia <a.segovia@hookto.fr>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,26 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
    compatibilityDate: '2025-07-15',
-   modules: ['nuxt-auth-utils', '@nuxt/eslint'],
+   modules: ['nuxt-auth-utils', '@nuxt/eslint', '@nuxtjs/i18n'],
    devtools: { enabled: true },
+   build: {
+      transpile: ['vuetify'],
+   },
+   vite: {
+      plugins: [
+         // @ts-expect-error
+         vuetify({ autoImport: true }),
+      ],
+      vue: {
+         template: {
+            transformAssetUrls,
+         },
+      },
+   },
+   i18n: {
+      locales: [{ code: 'fr-FR', iso: 'fr-FR', name: 'Français', file: 'fr-FR.ts' }],
+      langDir: 'locales/',
+      defaultLocale: 'fr-FR',
+   },
 })
