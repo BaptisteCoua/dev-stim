83896d14c7c67afddd2fbe0c99d8424a14abf02e
aurore.segovia <a.segovia@hookto.fr>
2026-04-17 09:39:19 +0200

    chore: add vuetify and its defaults file

---
+++ app/plugins/vuetify.ts
@@ -0,0 +1,13 @@
+// import this after install `@mdi/font` package
+import '@mdi/font/css/materialdesignicons.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import defaults from './vuetify/defaults'
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    defaults,
+  })
+  app.vueApp.use(vuetify)
+})
