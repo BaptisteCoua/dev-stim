da9aa193d276f8b7bacdcf76c3044a921eae452d
BaptisteCoua <couavoux.b@gmail.com>
2026-04-17 11:21:15 +0200

    chore: restart CI

--- server/api/webhook.post.ts
+++ server/api/webhook.post.ts
@@ -1,6 +1,8 @@
 export default defineEventHandler(async (event) => {
    const body = await readBody(event)
 
+   console.log('-------------------------------------------------')
+   console.log('-------------------------------------------------')
    console.log('Webhook reçu:', body)
 
    // TODO: vérifier signature si nécessaire
