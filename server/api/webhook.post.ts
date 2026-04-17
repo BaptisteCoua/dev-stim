4447810b0368a0055bfb22dcb63cd5456c0d7aa7
Baptiste <couavoux.b@gmail.com>
2026-04-17 11:39:09 +0200

    Feature/add webhook config (#2)
    * feat : ajouter un gestionnaire d'événements pour le webhook
    
    * chore: restart CI
    
    * chore: corriger le nom des workflows ESLint et Prettier
    
    * test
    
    * a
    
    * chore: mettre à jour les branches pour les déclencheurs de workflow ESLint
    
    * chore: simplifier la configuration des plugins Vite dans nuxt.config.ts

---
+++ server/api/webhook.post.ts
@@ -0,0 +1,12 @@
+export default defineEventHandler(async (event) => {
+   const body = await readBody(event)
+
+   console.log('-------------------------------------------------')
+   console.log('Webhook reçu:', body)
+
+   // TODO: vérifier signature si nécessaire
+
+   return {
+      status: 'ok',
+   }
+})
