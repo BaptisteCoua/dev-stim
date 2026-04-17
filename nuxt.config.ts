2b1577e2c90ce46cc56cfcd6845f0ded682898b0
RuddyMo <ruddy.morel@outlook.com>
2026-04-17 16:39:56 +0200

    chore: delete drizzle for prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,6 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
    compatibilityDate: '2025-07-15',
-
-   runtimeConfig: {
-      runningInDocker: process.env.RUNNING_IN_DOCKER,
-      databaseUrlLocal: process.env.DATABASE_URL_LOCAL,
-      databaseUrlDocker: process.env.DATABASE_URL_DOCKER,
-      public: {
-         apiBase: '/api'
-      }
-   },
    modules: ['nuxt-auth-utils', '@nuxt/eslint', '@nuxtjs/i18n'],
    devtools: { enabled: true },
    build: {
