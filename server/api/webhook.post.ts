3cab9bbd875bd7c8ec5502db547243307b3bdbab
BaptisteCoua <couavoux.b@gmail.com>
2026-04-17 11:27:27 +0200

    a

--- server/api/webhook.post.ts
+++ server/api/webhook.post.ts
@@ -2,7 +2,6 @@ export default defineEventHandler(async (event) => {
    const body = await readBody(event)
 
    console.log('-------------------------------------------------')
-   console.log('-------------------------------------------------')
    console.log('Webhook reçu:', body)
 
    // TODO: vérifier signature si nécessaire
