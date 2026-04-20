f15eeac7fc8364b9fb698b555d802a39a5f6c039
aurore.segovia <a.segovia@hookto.fr>
2026-04-20 16:18:59 +0200

    chore: delete unused storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,4 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook'
-
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
