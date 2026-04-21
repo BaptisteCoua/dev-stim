65438964be69ac079b03ce8b04ec52acf7e80d34
Aurore S <161458137+AuSegovia@users.noreply.github.com>
2026-04-21 11:22:22 +0200

    Chore/install echarts toastify and swiper (#14)
    * chore: init toastify
    
    * chore: init swiper
    
    * chore: init nuxt-echarts
    
    * Apply Prettier format
    
    * chore: delete .pnpm-store
    
    * Apply Prettier format
    
    * chore: fix lint
    
    * Apply Prettier format
    
    * feat: add exec command to prettier script in package.json
    
    * chore: fix lint warning
    
    ---------
    
    Co-authored-by: aurore.segovia <a.segovia@hookto.fr>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Clement <c.bilger@hookto.fr>
    Co-authored-by: BaptisteCoua <couavoux.b@gmail.com>

--- server/api/webhook.post.ts
+++ server/api/webhook.post.ts
@@ -1,12 +1,10 @@
 export default defineEventHandler(async (event) => {
    const body = await readBody(event)
 
-   console.log('-------------------------------------------------')
-   console.log('Webhook reçu:', body)
-
    // TODO: vérifier signature si nécessaire
 
    return {
+      body: body,
       status: 'ok',
    }
 })
