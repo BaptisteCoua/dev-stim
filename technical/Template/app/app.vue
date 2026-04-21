2d098314cfea50ac7e366a76fa6ef634adeee41a
aurore.segovia <a.segovia@hookto.fr>
2026-04-21 15:25:09 +0200

    chore: delete harmful script in app.vue

--- technical/Template/app/app.vue
+++ technical/Template/app/app.vue
@@ -4,13 +4,3 @@
       <NuxtPage />
    </v-app>
 </template>
-
-<script setup lang="ts">
-import { useThemeConfig } from '@core/composable/useThemeConfig'
-
-const { syncInitialLoaderTheme, syncVuetifyThemeWithTheme: syncConfigThemeWithVuetifyTheme } =
-  useThemeConfig()
-
-syncInitialLoaderTheme()
-syncConfigThemeWithVuetifyTheme()
-</script>
