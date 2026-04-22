cc8ed57f3a29aa27c368314508c7b16021299424
BaptisteCoua <couavoux.b@gmail.com>
2026-04-22 11:03:50 +0200

    feat: synchroniser les utilisateurs Jira depuis le webhook dans tous les événements

--- server/api/webhook.post.ts
+++ server/api/webhook.post.ts
@@ -3,11 +3,12 @@ import { syncJiraUsersFromWebhook } from '~/server/services/jira/sync-jira-users
 export default defineEventHandler(async (event) => {
    const body = await readBody(event)
 
+   await syncJiraUsersFromWebhook(body)
+
    switch (body?.webhookEvent) {
       case 'jira:issue_created':
       case 'jira:issue_updated':
-         // request
-         await syncJiraUsersFromWebhook(body)
+         // futur code
          break
       default:
          console.log(`[webhook] Unhandled event: ${body?.webhookEvent}`)
