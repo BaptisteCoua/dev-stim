7e6db8c23e0b3dc51b281ce66056d6cc9f15dfe1
rmorel <ru.morel@hookto.fr>
2026-04-28 09:57:56 +0200

    fix: bug merge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,9 +22,4 @@ export default defineOSDDNuxtConfig({
    compatibilityDate: '2025-07-15',
    modules: ['nuxt-auth-utils', '@nuxt/eslint', '@nuxtjs/i18n', 'nuxt-echarts'],
    devtools: { enabled: true },
-   i18n: {
-      locales: [{ code: 'fr-FR', iso: 'fr-FR', name: 'Français', file: 'fr-FR.ts' }],
-      langDir: 'locales/',
-      defaultLocale: 'fr-FR',
-   },
 })
