4e646bfb775181dec12029f6c8139784a5a8544b
aurore.segovia <a.segovia@hookto.fr>
2026-04-30 16:29:11 +0200

    feat: create Pie Chart for gradient card and linked stories

--- functional/KPI/app/components/GradientCard/GradientCard.vue
+++ functional/KPI/app/components/GradientCard/GradientCard.vue
@@ -1,31 +1,44 @@
 <template>
-   <v-card class="px-5 pt-5 pb-1 text-textColor rounded-lg" :class="gradientUsed">
-      <v-card-title
-         class="text-textColor text-title-large pa-0 font-weight-semibold card--title-height"
-      >
-         {{ title }}
-      </v-card-title>
-      <v-card-subtitle
-         class="text-textColor font-weight-bold text-display-large pa-0 card--subtitle-height"
-         opacity="100"
-      >
-         {{ mainInformation }}
-      </v-card-subtitle>
-      <v-card-text
-         class="text-textColor text-title-medium pa-0 font-weight-medium card--text-height"
-      >
-         {{ additionalInformation }}
-      </v-card-text>
+   <v-card
+      class="px-5 pt-5 pb-1 text-textColor rounded-lg d-flex align-center"
+      width="300px"
+      :class="gradientUsed"
+   >
+      <div>
+         <v-card-title
+            class="text-textColor text-title-large pa-0 font-weight-semibold card--title-height"
+         >
+            {{ title }}
+         </v-card-title>
+         <v-card-subtitle
+            class="text-textColor font-weight-bold text-display-large pa-0 card--subtitle-height"
+            opacity="100"
+         >
+            {{ mainInformation }}
+         </v-card-subtitle>
+         <v-card-text
+            class="text-textColor text-title-medium pa-0 font-weight-medium card--text-height"
+         >
+            {{ additionalInformation }}
+         </v-card-text>
+      </div>
+      <div v-if="numberUsDone && totalUs">
+         <PieChart :number-us-done :total-us />
+      </div>
    </v-card>
 </template>
 
 <script setup lang="ts">
+   import PieChart from '~/functional/KPI/app/components/PieChart/PieChart.vue'
+
    const props = withDefaults(
       defineProps<{
          gradientChosen?: string | null
          title: string
          mainInformation: string
          additionalInformation?: string
+         numberUsDone?: number
+         totalUs?: number
       }>(),
       { gradientChosen: 'info', additionalInformation: '' },
    )
---
+++ functional/KPI/app/components/PieChart/PieChart.vue
@@ -0,0 +1,54 @@
+<template>
+   <div class="d-flex flex-wrap justify-center align-center pie-chart-appearance">
+      <v-chart :option="option" autoresize> </v-chart>
+      <div
+         class="position-absolute w-100 d-flex flex-column align-center justify-center text-center chart-overlay text-textColor font-weight-bold text-title-large"
+      >
+         {{ PERCENTAGE }}
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+   const props = defineProps<{
+      numberUsDone: number
+      totalUs: number
+   }>()
+
+   const PERCENTAGE = computed(() => {
+      const response = (props.numberUsDone / props.totalUs) * 100
+      return `${Math.trunc(response)}%`
+   })
+
+   const option = {
+      tooltip: {
+         show: false,
+      },
+      series: [
+         {
+            name: 'Access From',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+               show: false,
+            },
+            labelLine: {
+               show: false,
+            },
+            data: [
+               { value: props.numberUsDone, name: 'Done' },
+               { value: props.totalUs - props.numberUsDone, name: 'ToDo' },
+            ],
+            color: ['rgba(255, 255, 255, 0.77)', 'rgba(250, 250, 253, 0.39)'],
+         },
+      ],
+   }
+</script>
+
+<style>
+   .pie-chart-appearance {
+      height: 120px;
+      width: 120px;
+   }
+</style>
